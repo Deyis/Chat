3f0d53a6023ca39f3548f3fa45e58d28cee2d6b1
Denis_Ivanchenko <denisivanchenko1991@gmail.com>
2014-09-12 16:49:21 +0200

    try to make ajax request to home controller

--- src/main/webapp/resources/js/some.js
+++ src/main/webapp/resources/js/some.js
@@ -1,3 +1,10 @@
 window.onload=function(){
     $(".cool").append("<h1>The Coolest Chat Ever</h1>");
+
+    $.ajax({
+        type: "GET",
+        url: "./hello.json"
+    }).done(function(data) {
+        $(".cool").append("<h1>"+ data.greeting +"</h1>");
+    });
 }
