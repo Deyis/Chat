cd6fd999dd2d9e348831b8d13fe7b64dda863189
Denis_Ivanchenko <denisivanchenko1991@gmail.com>
2014-09-18 16:49:27 +0200

    add angular js

---
+++ web/src/main/webapp/resources/js/app.js
@@ -0,0 +1,11 @@
+(function(){
+    var app = angular.module('store', [ ]);
+
+    app.controller('LoginController', function(){
+
+    });
+
+    app.controller('HelloController', function(){
+
+    });
+})();
