6e33c71f06d9aeb95e3fa5660436f3ebc127bf69
Denis_Ivanchenko <denisivanchenko1991@gmail.com>
2014-10-10 15:33:14 +0200

    add argument checking

--- common/src/main/java/com/hifive/common/util/StreamUtil.java
+++ common/src/main/java/com/hifive/common/util/StreamUtil.java
@@ -11,6 +11,10 @@ import java.util.stream.StreamSupport;
 public class StreamUtil {
 
     public static <K, V> Stream<Pair<K, V>> getPairStream(List<Object> list) {
+        if (list.size() % 2 == 0) {
+            throw new IllegalArgumentException("Length must be even");
+        }
+
         Iterator<Object> it = list.iterator();
         Iterable<Pair<K, V>> i = () -> new Iterator<Pair<K, V>>() {
 
